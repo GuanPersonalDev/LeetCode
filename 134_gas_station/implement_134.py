80d832b5487860f9d5b84b903acc39d86c38ad3c
GuanMac <guanpersonalproject@gmail.com>
2026-02-19 09:15:37 +0900

    optmize: simplify implement_134

--- 134_gas_station/implement_134.py
+++ 134_gas_station/implement_134.py
@@ -2,22 +2,16 @@ from typing import List
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        remain_right = 0
-        remain_left = 0
-        start_index = -1
+        if sum(cost) > sum(gas):
+            return -1
+        start_index = 0
+        tank = 0
         for i in range(0, len(gas)):
-            offset = gas[i] - cost[i]
-            if offset >= 0:
-                if start_index == -1:
-                    start_index = i
-            remain_right += offset
-            if remain_right < 0:
-                start_index = -1
-                remain_left += remain_right
-                remain_right = 0
-        if remain_right + remain_left >= 0:
-            return start_index
-        return -1
+            tank += gas[i] - cost[i]
+            if tank < 0:
+                start_index = i+1
+                tank = 0
+        return start_index
 
 solution = Solution()
 test_gas = [1, 2, 3, 4, 5]
@@ -30,4 +24,8 @@ print(solution.canCompleteCircuit(test_gas, test_cost))
 
 test_gas = [5, 8, 2, 8]
 test_cost = [6, 5, 6, 6]
+print(solution.canCompleteCircuit(test_gas, test_cost))
+
+test_gas = [3, 1, 1]
+test_cost = [1, 2, 2]
 print(solution.canCompleteCircuit(test_gas, test_cost))
